0a47358fe2b018143790d75e45f25fc5ba44d17d
Denis Kuznetsov <kyznecov995@icloud.com>
2023-12-31 16:41:19 +0400

    add React js render

--- script.js
+++ script.js
@@ -1,92 +1,153 @@
-// const data = {"sources":[{"id":1,"name":"meduza"},{"id":6,"name":"3dnews"},{"id":7,"name":"nytimes"},{"id":8,"name":"forbes"},{"id":9,"name":"igromania"},{"id":10,"name":"buro237"},{"id":11,"name":"rusvesna"},{"id":12,"name":"7ya"},{"id":14,"name":"aif"},{"id":15,"name":"gazetaru"},{"id":16,"name":"karpov.courses"}],"categories":[{"id":1,"name":"tech"},{"id":2,"name":"sport"},{"id":3,"name":"fashion"},{"id":4,"name":"politics"},{"id":5,"name":"other"},{"id":6,"name":"karpov.courses"}],"items":[{"id":479551,"lang":"ru","date":"Sat Dec 30 2023 02:23:57 GMT+0300 (Moscow Standard Time)","title":"JPL: аппарат \"Юнона\" окажется к Ио ближе, чем когда бы то ни было за 20 лет","description":"Космическая исследовательская станция NASA \"Юнона\" подойдет к одной из лун Юпитера Ио на самое близкое расстояние с начала своей миссии, стартовавшей в 2011 году. Событие произойдет 30 декабря, сообщили на сайте Лаборатории ...","image":"http://img.gazeta.ru/files3/569/14378569/upload-pia21770-pic905-895x505-92561.jpg","source_id":15,"category_id":1},{"id":479536,"lang":"ru","date":"Sat Dec 30 2023 02:13:40 GMT+0300 (Moscow Standard Time)","title":"Небензя: украинские власти размещают системы ПВО в жилых районах","description":"Кроме того, постпред РФ при ООН подчеркнул, что Украина превратилась в ЧВК.","image":"https://aif-s3.aif.ru/images/035/330/f677a1822f0ef48529c71a5b88876377.jpg","source_id":14,"category_id":4},{"id":479541,"lang":"ru","date":"Sat Dec 30 2023 02:13:37 GMT+0300 (Moscow Standard Time)","title":"Россия предупредила Запад о плохих новостях с Украины в ближайшее время","description":"Угроза, создаваемая странами НАТО с территории Украины, будет безусловно устранена военными или дипломатическими методами. Об этом на заседании Совбеза ООН заявил постоянный представитель России при международной организации Василий ...","image":"https://img.gazeta.ru/files3/443/16079443/20200713_gaf_ic06_061-pic905-895x505-99446.jpg","source_id":15,"category_id":4},{"id":479537,"lang":"ru","date":"Sat Dec 30 2023 02:12:05 GMT+0300 (Moscow Standard Time)","title":"Во Всеволожске госпитализировали ребенка, отравившегося каплями от насморка","description":"Препарат был противопоказан ребенку по возрасту.","image":"https://aif-s3.aif.ru/images/035/330/f6b03574e24a80118882bd2f491703b3.jpg","source_id":14,"category_id":5},{"id":479543,"lang":"ru","date":"Sat Dec 30 2023 02:02:17 GMT+0300 (Moscow Standard Time)","title":"США предложили Польше техническое содействие в расследовании инцидента с ракетой","description":"Советник президента США Джо Байдена по национальной безопасности Джейк Салливан заявил, что Соединенные Штаты готовы предложить Польше необходимое техническое содействие в расследовании инцидента с ракетой. Об этом ...","image":"https://img.gazeta.ru/files3/871/15741871/AP22273704721204-pic905-895x505-43188.jpg","source_id":15,"category_id":4},{"id":479552,"lang":"ru","date":"Sat Dec 30 2023 02:00:33 GMT+0300 (Moscow Standard Time)","title":"Shanghai Observer: КНР развернет на орбите сеть из 12 тыс. интернет-спутников","description":"Первый спутник проекта G60 Starlink выпустили в Китае на заводе в районе Сунцзян города Шанхая. Аппарат станет частью будущей сети интернет-связи, которая составит конкуренцию американскому Starlink компании SpaceX миллиардера Илона Маска. ...","image":"http://img.gazeta.ru/files3/425/14561425/Depositphotos_534071944_XL-pic905-895x505-23063.jpg","source_id":15,"category_id":1},{"id":479538,"lang":"ru","date":"Sat Dec 30 2023 01:42:26 GMT+0300 (Moscow Standard Time)","title":"Власти Саудовской Аравии сорвали матч за Суперкубок Турции","description":"Игра была отменена за десять минут до начала из-за футболок с портретом первого турецкого лидера Мустафы Кемаля Ататюрка.","image":"https://aif-s3.aif.ru/images/035/330/148867a4f282eeedd22b80b418a27c2e.jpg","source_id":14,"category_id":2},{"id":479553,"lang":"ru","date":"Sat Dec 30 2023 01:31:19 GMT+0300 (Moscow Standard Time)","title":"Nano Today: наночастицы превращают раковые ткани в мишени для иммунных клеток","description":"Южнокорейские ученые из Ульсанского национального института науки и технологий (UNIST) разработали технологию для выборочного уничтожения раковых опухолей с помощью белковых частиц наноразмера. Исследование опубликовано в научном ...","image":"http://img.gazeta.ru/files3/395/16619395/shutterstock_639636403-pic905-895x505-97800.jpg","source_id":15,"category_id":1},{"id":479539,"lang":"ru","date":"Sat Dec 30 2023 01:18:58 GMT+0300 (Moscow Standard Time)","title":"В Харьковской области прозвучала серия взрывов","description":"На территории региона объявлена воздушная тревога.","image":"https://aif-s3.aif.ru/images/035/330/86d6bdfe5f1d0d6dd630262a034e94cb.jpg","source_id":14,"category_id":5},{"id":479547,"lang":"ru","date":"Sat Dec 30 2023 01:10:00 GMT+0300 (Moscow Standard Time)","title":"NVIDIA выпустила драйвер с поддержкой видеокарты GeForce RTX 4090D","description":"Компания NVIDIA выпустила свежий пакет графического драйвера GeForce Game Ready 546.34 WHQL. Ключевым нововведением нового программного обеспечения является поддержка флагманской видеокарты GeForce RTX 4090D, выпущенной специально для Китая, а также других стран, попавших под экспортные ограничения США.","image":"https://3dnews.ru/assets/external/illustrations/2023/12/29/1098208/NVIDIA-GeForce-RTX-4090-D-GPU-1.jpg","source_id":6,"category_id":1},{"id":479540,"lang":"ru","date":"Sat Dec 30 2023 00:57:02 GMT+0300 (Moscow Standard Time)","title":"Над территорией Брянска сбили два беспилотника","description":"По предварительным данным, пострадавших нет.","image":"https://aif-s3.aif.ru/images/035/330/81047cbf9feffdb3f5fb9e5403fb7b7c.jpg","source_id":14,"category_id":5},{"id":479542,"lang":"ru","date":"Sat Dec 30 2023 00:51:35 GMT+0300 (Moscow Standard Time)","title":"Гладков: в Белгороде при попадании снаряда в частный дом погиб мужчина","description":"Кроме того, по словам губернатора Белгородской области, в результате атаки пострадали четыре человека, в том числе ребёнок.","image":"https://aif-s3.aif.ru/images/035/330/c8c7078e3d7266c9a189c2f0aa2350d8.jpg","source_id":14,"category_id":5},{"id":479544,"lang":"ru","date":"Sat Dec 30 2023 00:47:58 GMT+0300 (Moscow Standard Time)","title":"Захарова выразила соболезнования в связи с убийством отца Владимира Легойды","description":"Тело 90-летнего Романа Легойды было обнаружено в Видном.","image":"https://aif-s3.aif.ru/images/035/330/c279a596fa08f8cf6c49da9f76ce7294.jpg","source_id":14,"category_id":4},{"id":479554,"lang":"ru","date":"Sat Dec 30 2023 00:41:43 GMT+0300 (Moscow Standard Time)","title":"Стала известна диета жителей первых городов Европы, возникших на территории Украины","description":"Население первых городов Европы на территориях, которые сейчас принадлежат Украине и Молдове, придерживалось преимущественно вегетарианской диеты. Это показало исследование, опубликованное в журнале PNAS.\n\nУченые провели раскопки первых ...","image":"https://img.gazeta.ru/files3/135/12514135/Depositphotos_3407162_l-2015-pic905-895x505-75640.jpg","source_id":15,"category_id":1},{"id":479555,"lang":"ru","date":"Sat Dec 30 2023 00:40:13 GMT+0300 (Moscow Standard Time)","title":"Врачи рассказали о способе избавления от боли при неизлечимом раке","description":"Лучевая терапия применяется не только для борьбы с раковыми клетками, но и для облегчения мучительных болей, причем эффект становится заметен в течение нескольких недель после начала лечения. Об этом онколог клиники Мэйо Адам Хольцман ...","image":"https://img.gazeta.ru/files3/263/12431263/Depositphotos_239281598_l-2015-pic905-895x505-66295.jpg","source_id":15,"category_id":1},{"id":479556,"lang":"ru","date":"Sat Dec 30 2023 00:37:55 GMT+0300 (Moscow Standard Time)","title":"Врачи назвали причины недостаточного кровоснабжения желчного пузыря","description":"Нарушение кровоснабжения желчного пузыря вызывается блокировкой одного сосуда (концевой артерии) и часто приводит к удалению органа. Исследование опубликовано в журнале BMJ eGastroenterology.\n\nВоспаление желчного пузыря чаще всего ...","image":"https://img.gazeta.ru/files3/430/16642430/shutterstock_285240626-pic905-895x505-23128.jpg","source_id":15,"category_id":1},{"id":479545,"lang":"ru","date":"Sat Dec 30 2023 00:34:08 GMT+0300 (Moscow Standard Time)","title":"СБУ обвинила владельца заводов по производству водки в пособничестве РФ","description":"Мужчине грозит до 12 лет лишения свободы.","image":"https://aif-s3.aif.ru/images/035/330/e66f72115399e3dbde695ae3f96791dd.jpg","source_id":14,"category_id":5},{"id":479546,"lang":"ru","date":"Sat Dec 30 2023 00:31:59 GMT+0300 (Moscow Standard Time)","title":"Ряду регионов России могут предоставить квоту на экспорт консервов","description":"Минэкономразвития РФ предлагает ввести до конца 2024 года тарифные квоты на экспорт из ряда регионов РФ консервов из рыбы и морепродуктов, пока определены показатели для Калининградской области. Об этом сообщает РИА Новости.\n\nВ публикации ...","image":"https://img.gazeta.ru/files3/277/17616277/RIA_5896327-pic905-895x505-9387.jpg","source_id":15,"category_id":4},{"id":479465,"lang":"ru","date":"Sat Dec 30 2023 00:06:57 GMT+0300 (Moscow Standard Time)","title":"Минобороны РФ: над Белгородской областью уничтожены 13 реактивных снарядов","description":"В ведомстве подчеркнули, что пресечена очередная попытка Киева совершить террористическую атаку.","image":"https://aif-s3.aif.ru/images/035/330/ce5d95884e4b0316182e161b85a306e8.jpg","source_id":14,"category_id":4},{"id":479531,"lang":"ru","date":"Sat Dec 30 2023 00:01:53 GMT+0300 (Moscow Standard Time)","title":"AdMa: свет позволяет передавать данные в 100 раз быстрее Wi-Fi-сетей","description":"Южнокорейские ученые из Пхонханского университета науки и технологий создали метод широкополосной передачи данных через видимый свет. Исследование опубликовано в научном журнале Advanced Materials (AdMa).\n\nПо словам специалистов, ...","image":"http://img.gazeta.ru/files3/163/14268163/Depositphotos_310724328_XL-pic905-895x505-38987.jpg","source_id":15,"category_id":1}]}
 
-let data = null
-
-const mainNewsContainer = document.querySelector('.articles__big-column')
-const smallNewsContainer = document.querySelector('.articles__small-column')
-
-/*<template id="main-article-item">
-    <article class="main-article">
-        <div class="main-article__image-container">
-            <img class="main-article__image" src="./images/image.jpg" alt="Фото новости">
-        </div>
-        <div class="main-article__content">
-            <span class="article-category main-article__category">Технологии</span>
-            <h2 class="main-article__title">Вещи, которые нужно знать перед стажировкой в IT сфере</h2>
-            <p class="main-article__text">Уличные музыканты продолжают радовать фанатов стрит арта. Для этого они исполняют привычные мелодии в новом формате!</p>
-            <span class="article-source main-article__source">Источник</span>
-        </div>
-    </article>
-</template>*/
-
-/*<template id="small-article-item">
-    <article class="small-article">
-        <h2 class="small-article__title">В американском зоопарке празднуют рождение гориллы: видео</h2>
-        <p class="small-article__caption">
-            <span class="article-date small-article__date">12 июля</span>
-            <span class="article-source small-article__source">Источник</span>
-        </p>
-    </article>
-</template> */
-
-const renderNews = (categoryId) => {
-    fetch('http://frontend.karpovcourses.net/api/v2/ru/news/' + (categoryId ? categoryId : ''))
-        .then(response => response.json())
-        .then(resData => {
-            data = resData
-
-            const mainNews = data.items.slice(0, 3)
-            const smallNews = data.items.slice(3, 12)
-
-            mainNews.forEach(item => {
-                const categoryData = data.categories.find(itemCategory => itemCategory.id === item.category_id)
-                const sourceData = data.sources.find(itemSource => itemSource.id === item.source_id)
-
-                const template = document.createElement('template')
-
-                template.innerHTML = `
-        <article class="main-article">
-            <div class="main-article__image-container">
-                <img class="main-article__image" src="${item.image}" alt="${item.title}">
-            </div>
-            <div class="main-article__content">
-                <span class="article-category main-article__category">${categoryData.name}</span>
-                <h2 class="main-article__title">${item.title}</h2>
-                <p class="main-article__text">${item.description}</p>
-                <span class="article-source main-article__source">${sourceData.name}</span>
-            </div>
-        </arti
-    `
-
-                mainNewsContainer.appendChild(template.content)
-            })
-
-            smallNews.forEach(item => {
-                const sourceData = data.sources.find(itemSource => itemSource.id === item.source_id)
-                const dateData = new Date(item.date).toLocaleDateString('ru-RU', {month: 'long', day: 'numeric'})
-
-                const template = document.createElement('template')
-
-                template.innerHTML = `
-         <article class="small-article">
-            <h2 class="small-article__title">${item.title}</h2>
-            <p class="small-article__caption">
-                <span class="article-date small-article__date">${dateData}</span>
-                <span class="article-source small-article__source">${sourceData.name}</span>
-            </p>
-        </article>
-    `
-
-                smallNewsContainer.appendChild(template.content)
-            })
-
-        })
-        .catch(e => console.log(e))
+const categoryName = {
+    'index' : 'главная',
+    'fashion' : 'Мода',
+    'tech' : 'технологии',
+    'politics' : 'политика',
+    'sport' : 'спорт'
 }
 
+const categoryId = {
+    'index' : 0,
+    'fashion' : 3,
+    'tech' : 1,
+    'politics' : 4,
+    'sport' : 2
+}
 
-// минус в данном способе html инъекции мы можем заэнкодить строку,
-// но при этом если нам бэк сам присылает например готовую разметку для каких-то вариантов текста или элементов,
-// то мы его разметку заэнкодим.
+const Navigation = ({onClickNav, currentCategory, className = ''}) => {
+    return (
+        <nav className={"navigation grid " + className}>
+            <a href="#" className="navigation__logo" onClick={onClickNav} data-href='index'>
+                <img className="navigation__image" src="./images/logo.svg" alt="Логотип" />
+            </a>
+            <ul className="navigation__list">
+                {['index', 'fashion', 'tech', 'politics', 'sport'].map(item => (
+                    <li className="navigation__item" key={item}>
+                        <a
+                            onClick={onClickNav}
+                            href="#"
+                            data-href={item}
+                            className={`navigation__link ${currentCategory === item ? 'navigation__link--active' : '' }`}
+                        >
+                            {categoryName[item]}
+                        </a>
+                    </li>
+                ))}
+            </ul>
+        </nav>
+    )
+}
 
-// как заэнкодить код в js https://gomakethings.com/how-to-encode-strings-with-vanilla-js-to-reduce-the-risk-of-xss-attacks/
+const MainArticle = ({image, title, category, description, source}) => {
+    return (
+        <article className="main-article">
+            <div className="main-article__image-container">
+                <img className="main-article__image" src={image} alt={title}/>
+            </div>
+            <div className="main-article__content">
+                <span className="article-category main-article__category">
+                    {category}
+                </span>
+                <h2 className="main-article__title">{title}</h2>
+                <p className="main-article__text">{description}</p>
+                <span className="article-source main-article__source">
+                    {source}
+                </span>
+            </div>
+        </article>
+    )
+}
+
+const SmallArticle = ({title, date, source}) => {
+    return (
+        <article className="small-article">
+            <h2 className="small-article__title">{title}</h2>
+            <p className="small-article__caption">
+                <span className="article-date small-article__date">
+                    {new Date(date).toLocaleDateString('ru-RU', {
+                        month: 'long',
+                        day: 'numeric'})
+                    }
+                </span>
+                <span className="article-source small-article__source">
+                    {source}
+                </span>
+            </p>
+        </article>
+    )
+}
+
+const App = () => {
+    const [category, setCategory] = React.useState('index')
+    const [articles, setArticles] = React.useState({ items: [], categories: [], sources: []})
+
+    const onClickNav = (e) => {
+        e.preventDefault()
+        setCategory(e.currentTarget.dataset.href)
+    }
+
+    React.useEffect(() => {
+        fetch('https://frontend.karpovcourses.net/api/v2/ru/news/' + (categoryId[category] || ''))
+            .then(response => response.json())
+            .then(resData => {
+                setArticles(resData)
+            })
+            .catch(e => console.log(e))
+    }, [category])
+
+    return (
+        <>
+            <header className="header">
+                <div className="container">
+                    <Navigation
+                        className="header__navigation"
+                        onClickNav={onClickNav}
+                        currentCategory={category}/>
+                </div>
+            </header>
+
+            <main className="main">
+                <section className="articles">
+                    <div className="container grid">
+                        <section className="articles__big-column">
+                            {articles.items.slice(0, 3).map(item => {
+                                return <MainArticle
+                                    key={item.id}
+                                    category={articles.categories.find(({id}) => item.category_id === id).name}
+                                    source={articles.sources.find(({id}) => item.source_id === id).name}
+                                    {...item}
+                                />})
+                            }
+                        </section>
+
+                        <section className="articles__small-column">
+                            {articles.items.slice(3, 12).map(item => {
+                                return  <SmallArticle
+                                    key={item.id}
+                                    source={articles.sources.find(({id}) => item.source_id === id).name}
+                                    {...item}
+                                />})
+                            }
+                        </section>
+                    </div>
+                </section>
+            </main>
+
+            <footer className="footer">
+                <div className="container">
+                    <Navigation
+                        className="footer__navigation"
+                        onClickNav={onClickNav}
+                        currentCategory={category}/>
+                    <div className="footer__column">
+                        <p className="footer__text">CopyWrite</p>
+                        <p className="footer__copyright">© 2024</p>
+                    </div>
+                </div>
+            </footer>
+        </>
+    )
+}
+
+ReactDOM.render(<App/>, document.getElementById('root'))
