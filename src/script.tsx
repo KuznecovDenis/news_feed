52eeb6f08815198568ced326bb56b1c9db81fd67
Denis Kuznetsov <kyznecov995@icloud.com>
2024-02-11 19:20:13 +0400

    add: CRUD partner-article

--- src/script.tsx
+++ src/script.tsx
@@ -3,6 +3,9 @@ import { createRoot, Root } from 'react-dom/client';
 import './base.css';
 import { BrowserRouter } from 'react-router-dom';
 import { App } from './Components/App/App';
+import { initializeAPI } from './api';
+
+initializeAPI();
 
 const rootEl: null | HTMLElement = document.getElementById('root');
 if (!rootEl) throw new Error('Root is not find');
