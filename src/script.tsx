a47e0221e1bbdac0d0139d3e514a7da349f6d354
Denis Kuznetsov <kyznecov995@icloud.com>
2024-02-14 00:17:45 +0400

    add: auth admin

--- src/script.tsx
+++ src/script.tsx
@@ -4,8 +4,10 @@ import './base.css';
 import { BrowserRouter } from 'react-router-dom';
 import { App } from './Components/App/App';
 import { initializeAPI } from './api';
+import { AuthContextProvider } from './Features/Auth/AuthContextProvider';
 
 initializeAPI();
+const firebaseApp = initializeAPI();
 
 const rootEl: null | HTMLElement = document.getElementById('root');
 if (!rootEl) throw new Error('Root is not find');
@@ -13,8 +15,10 @@ if (!rootEl) throw new Error('Root is not find');
 const root: Root = createRoot(rootEl);
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <AuthContextProvider firebaseApp={firebaseApp}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </AuthContextProvider>
   </React.StrictMode>,
 );
