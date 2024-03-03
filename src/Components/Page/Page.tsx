e2f1344db1bfbccb635e1c2824980f570b4e7748
Denis Kuznetsov <kyznecov995@icloud.com>
2024-03-03 04:21:11 +0400

    add modal and dropdown-theme-color-switcher

--- src/Components/Page/Page.tsx
+++ src/Components/Page/Page.tsx
@@ -1,21 +1,34 @@
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
 import './Page.css';
 import { Navigation } from '../Navigation/Navigation';
 import { Logo } from '../Logo/Logo';
 import { ColorSchemeSwitcher } from '@components/ColorSchemeSwitcher/ColorSchemeSwitcher';
+import { EmailModal } from '@components/EmailModal/EmailModal';
+
+const LS_EMAIL_SHOWN_KEY = 'newsfeed:email_modal_shown';
 
 interface Props {
   children: React.ReactNode;
 }
 
 export const Page: FC<Props> = ({ children }) => {
+  const [emailModalShown, setEmailModalShown] = useState(!localStorage.getItem(LS_EMAIL_SHOWN_KEY));
+
   return (
     <>
+      {emailModalShown && (
+        <EmailModal
+          onClose={() => {
+            localStorage.setItem(LS_EMAIL_SHOWN_KEY, 'true');
+            setEmailModalShown(false);
+          }}
+        ></EmailModal>
+      )}
       <header className="header">
         <div className="container header__container">
           <Logo />
           <Navigation className="header__navigation" />
-          <div className="header__controls">
+          <div className="header__controls" style={{ transform: 'translateX(0)' }}>
             <ColorSchemeSwitcher />
           </div>
         </div>
